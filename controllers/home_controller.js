ae441062186988398584da51b542a4c629115e60
unknown <AKSHAY@DESKTOP-79MP8RE.mns.local>
2021-11-14 12:29:33 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req, res){
+    return res.send('<h1> Express Is Up For Codial! </h1> ')
+}
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 8000;
 
 
+// use express router
+app.use('/', require('./routes'))
+
+
 app.listen(port, function(err){
     if(err){
         // console.log('error',err);
@@ -11,5 +15,5 @@ app.listen(port, function(err){
     console.log(`Server is running on port :${port}`);
 
 
-    console.log("changes done");
+    
 });
